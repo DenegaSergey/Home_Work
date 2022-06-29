4ab686d9a5326a59edf106cae147f829541698eb
SergeyDenega <denegasergej@gmail.com>
2022-06-29 17:33:14 +0300

    Add Task1

---
+++ Lesson004/Task 1 HW04.Birthday/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_1_HW04.Birthday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Today;
+
+            Console.WriteLine("Enter your Birthday in DD-MM-YYYY format: ");
+
+            var bDay = Console.ReadLine();
+            var myDate = Convert.ToDateTime(bDay);
+
+            var age = today.Year - myDate.Year; //Calculate age.
+
+            
+            if (myDate.Date > today.AddYears(-age)) age--;
+
+            Console.WriteLine($"You are {age} years old"); //Displaying the result on the screen.
+
+            // HACK: This code is temporary and can be removed.
+            Console.ReadKey();
+
+
+        }
+    }
+}
