32e7d7ae7353af056349c32e53b90f0ae8b17b6a
SergeyDenega <denegasergej@gmail.com>
2022-06-29 17:33:39 +0300

    Add Task2

---
+++ Lesson004/Task 2 HW04.Separator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task 2 HW04.Separator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordUser = Console.ReadLine() ;
+
+            Console.Clear();
+
+            wordUser = wordUser.Replace(" ", string.Empty);
+            var result = String.Join(" ", wordUser.ToCharArray());
+
+            string[] word1 = result.Split(' ');
+            foreach (string text in word1)
+            {
+                Console.WriteLine(text);
+                Thread.Sleep(250);
+            }  
+            Console.ReadKey ();
+        }
+    }
+}
