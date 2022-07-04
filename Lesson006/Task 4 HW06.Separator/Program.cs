a19a27d6c2e86cf1b3bbc2e356384ad3024f828f
SergeyDenega <denegasergej@gmail.com>
2022-07-04 20:52:53 +0300

    Add Task4

---
+++ Lesson006/Task 4 HW06.Separator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task4HW06.Separator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "Не жалею, не зову, не плачу,;Все пройдет, как с белых яблонь дым.;Увяданья золотом охваченный,;Я не буду больше молодым.;";
+            {
+                string[] splitedArray = userInput.Split(';');
+                string poemLine = "";
+                for (int i = 0; i < splitedArray.Length; i++)
+                {
+                    poemLine += (splitedArray[i] + "\n").Trim(' ').Replace('о', 'а');
+                }
+                Console.WriteLine(poemLine);
+            }
+            {
+                string[] splitedArray = userInput.Split(' ', ';');
+                string poemColumn = "";
+                for (int i = 0; i < splitedArray.Length; i++)
+                {
+                    poemColumn += (splitedArray[i] + "\n").Trim(' ').Replace('о', 'а');
+                }
+                Console.WriteLine(poemColumn);
+            }
+            Console.ReadKey();
+        }
+    }
+}
