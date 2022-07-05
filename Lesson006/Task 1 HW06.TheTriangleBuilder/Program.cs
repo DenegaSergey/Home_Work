f6b14e7cf20ab500de1fdf8140fdca07617f9d42
SergeyDenega <denegasergej@gmail.com>
2022-07-05 23:10:45 +0300

    Add Task1

---
+++ Lesson006/Task 1 HW06.TheTriangleBuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1HW06.TheTriangleBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int value = 1;
+
+            for (int lines = userInput; lines >= 1; lines--)
+            {
+                for (int spaces = userInput - 1; spaces >= lines; spaces--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int colums = 1; colums <= lines; colums++)
+                {
+                    Console.Write(value);
+                    Console.Write(" ");
+                }
+                value++;
+                if (value == 10)
+                {
+                    value = 0;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
