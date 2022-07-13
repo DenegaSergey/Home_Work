2ecd40cd040729fd728104ceaf6c83629151f1a0
SergeyDenega <denegasergej@gmail.com>
2022-07-13 10:24:42 +0300

    Fixed and rewrote the code

--- Lesson007/Task 1 HW07.StringWorkerTwo/Program.cs
+++ Lesson007/Task 1 HW07.StringWorkerTwo/Program.cs
@@ -1,102 +1,115 @@
-﻿using System;
+﻿ using System;
 
 namespace Task1HW07.StringWorkerTwo
 {
     internal class Program
     {
+        static void WordReplacement(string[] arreyWords, string longWord, string shortestWord)
+        {
+            int indexMaxWord = Array.IndexOf(arreyWords, longWord);
+            int indexMinWord = Array.IndexOf(arreyWords, shortestWord);
+
+            Console.WriteLine(" Min index: " + indexMaxWord); //10
+            Console.WriteLine(" Max index: " + indexMinWord + "\n"); //6
+
+            string temp = arreyWords[10];
+            arreyWords[10] = arreyWords[6];
+            arreyWords[6] = temp;
+
+            {
+                int index = Array.IndexOf(arreyWords, longWord);
+
+                for (int i = index; i < arreyWords.Length - 1; i++)
+                {
+                    arreyWords[i] = arreyWords[i + 1];
+                }
+                Array.Resize(ref arreyWords, arreyWords.Length - 1);
+                Console.WriteLine(String.Join(" ", arreyWords) + "\n");
+            }
+        }
+        static string ShortestWord(string[] arreyWords)
+        {
+            string shortestWord = arreyWords[0];
+            int smallestLength = arreyWords[0].Length;
+            for (int i = 0; i < arreyWords.Length; i++)
+            {
+                if (arreyWords[i] != "" && arreyWords[i].Length < smallestLength)
+                {
+                    smallestLength = arreyWords[i].Length;
+                    shortestWord = arreyWords[i];
+                }
+            }
+            Console.WriteLine("Shortest word: " + shortestWord + "\n");
+            return shortestWord;
+        }
+        static string ArraySort(string userWords)
+        {
+            string[] arrey = userWords.Split(' ', ',', '.');
+
+            foreach (var sub in arrey)
+            {
+                Console.WriteLine(sub);
+            }
+
+            Array.Sort(arrey, (x, y) => x.Length.CompareTo(y.Length));
+            Array.Reverse(arrey);
+            Console.WriteLine(String.Join(" ", arrey) + "\n");
+            return userWords;
+        }
+        static string NumberOfLetters(string userWords)
+        {
+            int letters = 0;
+            for (int i = 0; i < userWords.Length; i++)
+            {
+                if (char.IsLetter(userWords[i]))
+                    letters++;
+            }
+            Console.WriteLine("The number of letters is: {0}", letters + "\n");
+            return letters.ToString();
+        }
+        static string NumberOfPunctuation(string userWords)
+        {
+            int punctuationCount = 0;
+            for (int i = 0; i < userWords.Length; i++)
+            {
+                if (Char.IsPunctuation(userWords[i]))
+                    punctuationCount++;
+            }
+            Console.WriteLine("Number of punctuation marks: " + punctuationCount + "\n");
+
+            return punctuationCount.ToString();
+        }
+        static string LongestWord(string[] arreyWords, string word)
+        {
+            int longestWord = 0;
+            foreach (String s in arreyWords)
+            {
+                if (s.Length > longestWord)
+                {
+                    word = s;
+                    longestWord = s.Length;
+                }
+            }
+            Console.WriteLine("Longest word: " + word + "\n");
+            return word;
+        }
+
         static void Main(string[] args)
         {
             string userWords = "Всегда пишите код так, будто сопровождать его будет склонный к насилию психопат, который знает, где вы живете.";
             string[] arreyWords = userWords.Split(new[] { " ", ",", "." }, StringSplitOptions.None);
             string word = "";
-            int longestWord = 0;
 
-            {  // длинное слово
-                foreach (String s in arreyWords)
-                {
-                    if (s.Length > longestWord)
-                    {
-                        word = s;
-                        longestWord = s.Length;
-                    }
-                }
-                Console.WriteLine("Longest word: " + word + "\n");
-            }
+            string longWord = LongestWord(arreyWords, word);
+            string shortestWord = ShortestWord(arreyWords);
 
-            {
-                //замена самого длинного слова на самое короткое и удаление длинного слова
-                string shortestWord = arreyWords[0];
-                int smallestLength = arreyWords[0].Length;
-                for (int i = 0; i < arreyWords.Length; i++)
-                {
-                    if (arreyWords[i] != "" && arreyWords[i].Length < smallestLength)
-                    {
-                        smallestLength = arreyWords[i].Length;
-                        shortestWord = arreyWords[i];
-                    }
-                }
-                Console.WriteLine("Shortest word: " + shortestWord + "\n");
+            WordReplacement( arreyWords,longWord,shortestWord);
 
-                int indexMax = Array.IndexOf(arreyWords, word);
-                int indexMin = Array.IndexOf(arreyWords, shortestWord);
+            string punctuation = NumberOfPunctuation(userWords);
+            string letters = NumberOfLetters(userWords);
+            string sort = ArraySort(userWords);
 
-                Console.WriteLine(" Min index: " + indexMin); //10
-                Console.WriteLine(" Max index: " + indexMax + "\n"); //6
-
-                string temp = arreyWords[10];
-                arreyWords[10] = arreyWords[6];
-                arreyWords[6] = temp;
-
-                {
-                    int index = Array.IndexOf(arreyWords, word);
-
-                    for (int i = index; i < arreyWords.Length - 1; i++)
-                    {
-                        arreyWords[i] = arreyWords[i + 1];
-                    }
-                    Array.Resize(ref arreyWords, arreyWords.Length - 1);
-                    Console.WriteLine(String.Join(" ", arreyWords) + "\n");
-                }
-            }
-
-            {
-                // количество пунктуации
-                int punctuationCount = 0;
-                for (int i = 0; i < userWords.Length; i++)
-                {
-                    if (Char.IsPunctuation(userWords[i]))
-                        punctuationCount++;
-                }
-                Console.WriteLine("Number of punctuation marks: " + punctuationCount + "\n");
-            }
-
-            {
-                //количество букв
-                int letters = 0;
-                for (int i = 0; i < userWords.Length; i++)
-                {
-                    if (char.IsLetter(userWords[i]))
-                        letters++;
-                }
-                Console.WriteLine("The number of letters is: {0}", letters + "\n");
-            }
-
-            {
-                //строка в массив с разделителем.
-                string[] arrey = userWords.Split(' ', ',', '.');
-
-                foreach (var sub in arrey)
-                {
-                    Console.WriteLine(sub);
-                }
-
-                //сортировка
-                Array.Sort(arrey, (x, y) => x.Length.CompareTo(y.Length));
-                Array.Reverse(arrey);
-                Console.WriteLine(String.Join(" ", arrey) + "\n");
-
-                Console.ReadKey();
-            }
+            Console.ReadKey();
 
         }
     }
