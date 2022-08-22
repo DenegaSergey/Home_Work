8ef57a822b5c218296dce62284734c7a48a01482
SergeyDenega <denegasergej@gmail.com>
2022-08-22 03:53:48 +0300

    Add HW011

---
+++ Lesson011/ConsoleAppTwo/ConsoleAppTwo.DAL/Repositories/BaggageInfoRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airport.Core.Models;
+
+namespace Airport.DAL.Repositories
+{
+    public class BaggageInfoRepository
+    {
+        private readonly BaggageInfo _baggage;
+
+        public BaggageInfoRepository()
+        {
+            _baggage = new BaggageInfo()
+            {
+                MinWight = 0,
+            };
+            _baggage = new BaggageInfo()
+            {
+                MaxWight = 15,
+            };
+        }
+
+        public int GetMaxWight()
+        {
+            var user = new User();
+            user.BaggageInfo.MaxWight = 300;
+
+            return _baggage.MaxWight;
+        }
+    }
+}
---
+++ Lesson011/ConsoleAppTwo/ConsoleAppTwo.DAL/Repositories/FlightInfoRepository.cs
@@ -0,0 +1,95 @@
+﻿using Airport.Core.Models;
+
+namespace Airport.DAL.Repositories
+{
+    public class FlightInfoRepository
+    {
+        private readonly List<FlightInfo> _flights;
+
+        public FlightInfoRepository()
+        {
+            _flights = new List<FlightInfo>()
+            {
+                new FlightInfo()
+                {
+                    Id = 1,
+                    Number = "MR-03",
+                    From = "Minsk",
+                    To = "Paris",
+                    DepartureTime = new DateTime(2022,7,24, 01, 00, 00)
+
+                },
+                new FlightInfo()
+                {
+                    Id = 2,
+                    Number = "CR-07",
+                    From = "Minsk",
+                    To = "Rom",
+                    DepartureTime = new DateTime(2022,7,23, 22, 00, 00)
+                },
+                new FlightInfo()
+                {
+                    Id = 3,
+                    Number = "TD-08",
+                    From = "Minsk",
+                    To = "Moscow",
+                    DepartureTime = new DateTime(2022,8,22, 04, 30, 00)
+                }
+            };
+        }
+
+        public void Create(FlightInfo item)
+        {
+            _flights.Add(item);
+        }
+
+        public void Create(List<FlightInfo> listOfItems)
+        {
+            foreach (var item in listOfItems)
+            {
+                _flights.Add(item);
+            }
+        }
+        public List<FlightInfo> GetAll()
+        {
+            return _flights;
+        }
+
+        public FlightInfo GetFirstOrDefaultById(int id)
+        {
+            foreach (var flight in _flights)
+            {
+                if (flight.Id == id)
+                {
+                    return flight;
+                }
+            }
+            return null!;
+        }
+
+        public void Update(FlightInfo item)
+        {
+            foreach (var flight in _flights)
+            {
+                if (flight.Id == item.Id)
+                {
+                    flight.Number = item.Number;
+                    flight.From = item.From;
+                    flight.To = item.To;
+                    flight.DepartureTime = item.DepartureTime;
+                }
+            }
+        }
+        
+        public void Delete(int id)
+        {
+            foreach (var flight in _flights)
+            {
+                if (flight.Id == id)
+                {
+                    _flights.Remove(flight);
+                }
+            }
+        }
+    }
+}
